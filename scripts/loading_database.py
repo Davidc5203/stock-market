d58b2a4661a54bd5a94fe94125dcbbaecb1af6d6
David Catinac <catinacjts7@gmail.com>
2026-01-28 16:08:02 -0500

    Loaded transformed data into SQLite Database

---
+++ scripts/loading_database.py
@@ -0,0 +1,12 @@
+import sqlite3
+import pandas as pd
+
+df = pd.read_csv("data/transformed_stock_data.csv")
+
+connection = sqlite3.connect("data/stock_data.db")
+
+df.to_sql('stocks', connection, if_exists='replace', index=False)
+
+connection.close()
+
+print("✅ Data successfully loaded into stock_data.db")
